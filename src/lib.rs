b02e6a37f4cd073d7b1770316f0310c1ad100753
Cristina Sánchez Sánchez <cristina.sanchez@mongodb.com>
2026-07-31 18:05:47 +0100

    wip: implement LocalDeployment

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,15 @@
 use pyo3::prelude::*;
 
+mod deployment;
 mod exceptions;
 
 #[pymodule]
 mod atlas_local {
     use pyo3::prelude::*;
+    
+    #[pymodule_export]
+    pub use crate::deployment::LocalDeployment;
 
-    #[pyfunction]
-    fn test_error(name: &str) -> PyResult<String> {
-        use crate::exceptions::IntoPyErr;
-
-
-        fn some_call(name: &str) -> Result<String, ::atlas_local::client::StartDeploymentError> {
-            Err(::atlas_local::client::StartDeploymentError::ContainerStart(format!(
-                "no such container: {name}"
-            )))
-        }
-
-        let started = some_call(name).map_err(IntoPyErr::into_pyerr)?;
-        Ok(started)
-    }
-
-    /// Formats the sum of two numbers as string.
-    #[pyfunction]
-    fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-        Ok((a + b).to_string())
-    }
 
     #[pymodule_init]
     fn init(module: &Bound<'_, PyModule>) -> PyResult<()> {
